65521a7f239fdfed409e8de950699d57b0748cb8
RTDevOps <RTDevOps@dmspro.vn>
2023-11-08 17:43:41 +0700

    window init

--- OpenGLTutorial1/src/main.cpp
+++ OpenGLTutorial1/src/main.cpp
@@ -2,11 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	cout << "Hello, World!" << endl;
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint()
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Tutorial", NULL, NULL);
+	if (window == NULL) {
+		cout << "Could not create window." << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Check if GLAD loaded properly
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize GLAD" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	// Set size when player resize the window
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }

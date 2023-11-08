c125c6956462747f65dca8773cb3afcbfb3ccfef
RTDevOps <RTDevOps@dmspro.vn>
2023-11-08 17:24:44 +0700

    first commit

---
+++ OpenGLTutorial1/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint()
+	return 0;
+}
